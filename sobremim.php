9bafef715e8f34df0bcf40bbffc19fc9ee75e155
Breno Vieira Sapucaia <108706002+BrenoSapucaia@users.noreply.github.com>
2024-10-27 12:56:39 -0300

    Add files via upload

---
+++ sobremim.php
@@ -0,0 +1,22 @@
+<?php 
+$itens = [
+    [ 'href' => 'projetos', 'texto' => 'Projetos ' ],
+    [ 'href' => 'projetos', 'texto' => 'Github' ],
+    [ 'href' => 'projetos','texto' => 'Linkedin' ],
+    [ 'href' => 'projetos','texto' => 'Twitter' ],
+];
+?>
+<section class="flex justify-between items-center py-7">
+            <div>
+                <h1 class="text-4xl font-bold text-cyan-600">Oi, meu nome é Breno!</h1>
+                <p class="mt-4 text-lg text-gray-300 max-w-lg">
+                    Falando um pouco sobre mim, sou um desenvolvedor web que adora criar coisas novas e aprender novas tecnologias. Especializado em PHP e HTML, mas também tenho conhecimento em outras linguagens como Rect,jS, e 3D em three.js.
+                </p>
+                <div class="mt-6 font-medium text-lg text-cyan-600">
+                    X F L Y
+                </div>
+            </div>
+            <!-- Foto -->
+            <div class="text-gray-400 font-bold a">
+               <img class="w-48 b border-2 border-gray-400 -mt-6 houver-animate-pulse" src="./assets/perfil2.png" alt="imagem do perfil">
+        </section>
